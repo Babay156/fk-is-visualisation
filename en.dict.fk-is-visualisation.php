c266e3a65269b46d9f3ca4c62e059a8de9c02090
Babay156 <babay156@gmail.com>
2022-05-16 13:18:38 +0300

    main

--- en.dict.fk-is-visualisation.php
+++ en.dict.fk-is-visualisation.php
@@ -8,9 +8,5 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
-	'UI:ISDashboard:Title' => 'Information System Dashboard',
-	// Shorter labels for the badges at the top of the overview
-	'UI:SynchroDashboard:Overview:DataSources' => 'Data Sources',
-	'Menu:ISDashboard' => 'Information System Dashboard',
 ));
 ?>
