9bc7a1f2b75141b8c2b23e438fe1a0a0488887e7
Babay156 <babay156@gmail.com>
2022-05-17 01:21:53 +0300

    main

--- module.fk-is-visualisation.php
+++ module.fk-is-visualisation.php
@@ -15,8 +15,7 @@ SetupWebPage::AddModule(
 		// Setup
 		//
 		'dependencies' => array(
-			'itop-config-mgmt/2.4.0',
-			'itop-service-mgmt/2.4.0',			
+		
 		),
 		'mandatory' => false,
 		'visible' => true,
