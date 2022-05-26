af2d27147d4de3ab30d5f47ea34c1fde6c3c83f3
Babay156 <babay156@gmail.com>
2022-05-26 21:33:56 +0300

    fix path

---
+++ js/fk-is-functions.js
@@ -0,0 +1,19 @@
+/* 
+ This Source Code Form is subject to the terms of the Mozilla Public License, 
+ v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+ obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+jQuery(window).load(resize_table);
+
+jQuery(window).resize(resize_table);
+
+function resize_table() {
+    jQuery("/fk-is-visualisation/css/dashletptview.css").each(function (index, element) {
+        var textSize = 100;
+        do {
+            jQuery(element).css('font-size', textSize);
+            textSize -= 3;
+        } while (jQuery(element).width() > jQuery(element).parent().width())
+    });
+}
