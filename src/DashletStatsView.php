964fb5da8f64d6d74036e857292d68713dec67c0
Babay156 <babay156@gmail.com>
2022-06-03 16:13:15 +0300

    add color classes

--- src/DashletStatsView.php
+++ src/DashletStatsView.php
@@ -37,8 +37,7 @@ class DashletStatsView{
 		$sLinkUrl = utils::GetAbsoluteUrlAppRoot()."pages/UI.php?operation=search&filter=".$sFilter = rawurlencode($this->sFilter->serialize());
 		$oPage->add(
 <<<HTML
-<div id="$sDashletId" class="dashlet-content fk-is-dashlet-stats">
-	$sHtmlColor
+<div id="$sDashletId" class="dashlet-content fk-is-dashlet-$sHtmlShortName">
 	<a href="$sLinkUrl">
 	<div class="fk-is-dashlet-stats--details--value">$sHtmlValue</div>
 	<div class="fk-is-dashlet-stats--details--shortname">$sHtmlShortName</div>
