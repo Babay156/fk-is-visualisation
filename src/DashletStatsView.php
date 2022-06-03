3cd0bbd166dce98f6aedebb16181891c4bbefb79
Babay156 <babay156@gmail.com>
2022-06-03 15:47:25 +0300

    fix style

--- src/DashletStatsView.php
+++ src/DashletStatsView.php
@@ -37,7 +37,7 @@ class DashletStatsView{
 		$sLinkUrl = utils::GetAbsoluteUrlAppRoot()."pages/UI.php?operation=search&filter=".$sFilter = rawurlencode($this->sFilter->serialize());
 		$oPage->add(
 <<<HTML
-<div id="$sDashletId" class="dashlet-content fk-is-dashlet-$sHtmlColor">
+<div id="$sDashletId" class="dashlet-content fk-is-dashlet-stats">
 	<a href="$sLinkUrl">
 	<div class="fk-is-dashlet-stats--details--value">$sHtmlValue</div>
 	<div class="fk-is-dashlet-stats--details--shortname">$sHtmlShortName</div>
